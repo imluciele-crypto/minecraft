2f974d17cecb47ec63ebb2cad878175f237443d6
imluciele-cripto <imluciele@gmail.com>
2025-11-03 08:39:23 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-function mudar_conteudo_elemento() { 
+function mudar_texto_elemento() {
 
-    document.getElementById("demo").innerHTML = "O clique está funcionando";
+  document.getElementById("demo").innerHTML = "O clique está funcionando"
+
+}
+
+function mudar_cor_elemento(){
+
+  const paragrafo = document.querySelector('#paragrafo');
+
+  paragrafo.addEventListener('click', ()=> {
+
+    const elemento_paragrafo = document.querySelector('#paragrafo');
+
+    elemento_paragrafo.style.color = 'red';
+
+  });
+
+}
+
+function mudar_estilo_textual(){
+
+  document.getElementById("elemento_strong").style.textDecorationLine = "line-through";
 }
